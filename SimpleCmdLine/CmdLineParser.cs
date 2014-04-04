94d2c98cf60a7fc674faaf037a59c3d93445278b
santosh joseph <santoshjoseph73@gmail.com>
2014-04-03 22:27:21 -0700

    Added help parsing, default values, bool options, illegal values,  more tests

--- SimpleCmdLine/CmdLineParser.cs
+++ SimpleCmdLine/CmdLineParser.cs
@@ -25,21 +25,15 @@ namespace SonicComputing
         public dynamic Opts = new DynamicDictionary();
         private Dictionary<string, bool> _required = new Dictionary<string, bool>();
         private Dictionary<string, string> _help = new Dictionary<string, string>();
-        private Dictionary<string, string> _longOptValues = new Dictionary<string, string>();
-        private Dictionary<string, string> _shortOptValues = new Dictionary<string, string>();
+        //private Dictionary<string, string> _longOptValues = new Dictionary<string, string>();
+        //private Dictionary<string, string> _shortOptValues = new Dictionary<string, string>();
         private List<string> _longOpts = new List<string>();
         private List<string> _shortOpts = new List<string>();
         private Dictionary<string, Type> _optTypes = new Dictionary<string, Type>();
-        private Dictionary<string, dynamic> _defaults = new Dictionary<string, dynamic>();
         private List<string> _longOptsSet = new List<string>();
         private List<string> _shortOptsSet = new List<string>();
 
-        class Option
-        {
-
-        }
-
-        public void Setup<T>(string spec, bool required = true, string helpMsg = "", T defaultValue = default(T))
+        public void Setup<T>(string spec, bool required = true, string helpMsg = "")
         {
             if (string.IsNullOrEmpty(spec))
                 throw new ArgumentException();
@@ -73,7 +67,6 @@ namespace SonicComputing
             _optTypes[longOptName] = typeof(T);
             _required[longOptName] = required;
             _help[longOptName] = helpMsg;
-            _defaults[longOptName] = defaultValue;
 
         }
 
@@ -85,6 +78,8 @@ namespace SonicComputing
             if(args.Contains("--help") || args.Contains("-h"))
                 throw new ParseException("Usage:", GenerateHelpMsg());
 
+            //convert short opts to long opts?
+
             for (int i = 0; i < args.Length; i++)
             {
                 if(IsLongOpt(args[i]))
@@ -92,12 +87,30 @@ namespace SonicComputing
                     var longOptName = GetLongOptName(args[i]);
                     if (!IsValidOpt(longOptName))
                         throw new ParseException("Invalid Option:", GenerateHelpMsg());
+                    if (_longOptsSet.Contains(longOptName))
+                        throw new ParseException("Option specfied twice:" + longOptName);
+
                     _longOptsSet.Add(longOptName);
 
-                    Opts.TrySetMember(new MyMemberBinder(longOptName), 
-                        GetValue(longOptName, args[i+1]));
+                    if (_optTypes[longOptName].Name == "Boolean")
+                    {
+                        Opts.TrySetMember(new MyMemberBinder(longOptName), true);
+                    }
+                    else
+                    {
+                        if (i+1 >= args.Length)
+                            throw new ParseException("Missing value for option:" + longOptName);
+                        Opts.TrySetMember(new MyMemberBinder(longOptName),
+                            GetValue(longOptName, args[i + 1]));
+                    }
                 }
             }
+
+             _optTypes.Where(x => x.Value.Name == "Boolean")
+                    .Select(x => x.Key)
+                    .Except(_longOptsSet)
+                    .ToList()
+                    .ForEach(x => Opts.TrySetMember(new MyMemberBinder(x), false));
 
             var requiredOpts = _required.Where(x => x.Value).Select(x => x.Key);
             foreach(var required in requiredOpts)
@@ -115,7 +128,14 @@ namespace SonicComputing
                 return value;
             }
             var method = type.GetMethod("Parse", new []{typeof(string)});
-            return method.Invoke(type, new []{value});
+            try
+            {
+                return method.Invoke(type, new[] { value });
+            }
+            catch(Exception)
+            {
+                throw new ParseException("Illegal value for option:" + optName);
+            }
         }
 
         private bool IsLongOpt(string option)
--- Tests/WhenParsingOptions.cs
+++ Tests/WhenParsingOptions.cs
@@ -65,5 +65,182 @@ namespace Tests
 
             Assert.AreEqual(3, sut.Opts.option);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Only_Help_Option_Specified()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<int>("option,o");
+            sut.Parse(new[] { "--help" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Help_Option_Specified_With_Other_Options()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<int>("option1,1");
+            sut.Setup<int>("option2,2");
+            sut.Parse(new[] { "--option1", "--help" });
+        }
+
+        [TestMethod]
+        public void Default_Value_For_Option_Set_And_Option_Not_Specified()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Opts.option1 = 3;
+
+            sut.Setup<int>("option1,1", false);
+            sut.Setup<int>("option2,2", false);
+            sut.Parse(new[] { "--option2", "55" });
+
+            Assert.AreEqual(3, sut.Opts.option1);
+            Assert.AreEqual(55, sut.Opts.option2);
+        }
+
+        [TestMethod]
+        public void Default_Value_For_Option_Set_And_Option_Specified()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Opts.option1 = "abc";
+
+            sut.Setup<string>("option1,1", false);
+            sut.Setup<int>("option2,2");
+            sut.Parse(new[] { "--option1", "def", "--option2", "55" });
+
+            Assert.AreEqual("def", sut.Opts.option1);
+            Assert.AreEqual(55, sut.Opts.option2);           
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Option_Specified_Twice_Throws_Exception()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<string>("option1,1");
+            sut.Setup<int>("option2,2", false);
+            sut.Parse(new[] { "--option1", "def", "--option1", "def" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Illegal_Value_For_Option_Specified()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<string>("option1,1");
+            sut.Setup<int>("option2,2");
+            sut.Parse(new[] { "--option1", "def", "--option2", "def" });           
+        }
+
+        [TestMethod]
+        public void Option_Specified_Boolean()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<bool>("option", false);
+
+            sut.Parse(new[] { "--option" });
+
+            Assert.AreEqual(true, sut.Opts.option);
+        }
+
+        [TestMethod]
+        public void Multiple_Boolean_Option_Specified()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<bool>("option1", false);
+            sut.Setup<bool>("option2", false);
+            sut.Setup<bool>("option3", false);
+
+            sut.Parse(new[] { "--option1" });
+
+            Assert.AreEqual(true, sut.Opts.option1);
+            Assert.AreEqual(false, sut.Opts.option2);
+            Assert.AreEqual(false, sut.Opts.option3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Single_Option_Specified_But_No_Value()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<int>("option", false);
+
+            sut.Parse(new[] { "--option" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Multiple_Options_Specified_But_No_Value()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<int>("option1");
+            sut.Setup<string>("option2");
+            sut.Setup<bool>("option3", false);
+
+            sut.Parse(new[] { "--option1", "--option2", "--option3" });
+        }
+
+        class Class1
+        {
+            public int Nothing { get; set; }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Custom_Type_Cannot_Parse()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<Class1>("option", false);
+
+            sut.Parse(new[] { "--option", "3" });
+        }
+
+        class Class2
+        {
+            public int MyProperty { get; set; }
+            void Parse(string input)
+            {
+                MyProperty = Int32.Parse(input);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParseException))]
+        public void Custom_Type_With_Parse_Method()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<Class2>("option", false);
+
+            sut.Parse(new[] { "--option", "3" });
+        }
+
+        [TestMethod]
+        public void Multiple_Options_Valid_CommandLine()
+        {
+            var sut = new CmdLineParser();
+
+            sut.Setup<int>("option1");
+            sut.Setup<string>("option2");
+            sut.Setup<bool>("option3", false);
+
+            sut.Parse(new[] { "--option1", "3", "--option2", "abc", "--option3" });
+
+            Assert.AreEqual(3, sut.Opts.option1);
+            Assert.AreEqual("abc", sut.Opts.option2);
+            Assert.AreEqual(true, sut.Opts.option3);
+        }
     }
 }
